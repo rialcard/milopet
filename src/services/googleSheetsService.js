9ced20b96d13745154a95a15ac9f4cb3b634cf73
rialcard <racardenasgarcia@gmail.com>
2025-04-01 06:40:39 -0500

    Correccion googleSheetsService error en replace

--- src/services/googleSheetsService.js
+++ src/services/googleSheetsService.js
@@ -28,7 +28,7 @@ const appendToSheet = async (data) => {
         const auth = new google.auth.GoogleAuth({
             credentials: {
                 client_email: process.env.CLIENT_EMAIL,
-                private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+                private_key: process.env.PRIVATE_KEY,
             },
             scopes: ['https://www.googleapis.com/auth/spreadsheets']
         });
